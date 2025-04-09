8602fc6901063ea02af142996cba5840f4ca5719
jo9182 <73309930+jo9182@users.noreply.github.com>
2025-04-09 13:29:45 +0000

    :(

---
+++ NumberGroup.java
@@ -0,0 +1,6 @@
+public interface NumberGroup
+{
+    public static final int[] numbers;
+
+    public contains;
+}
--- Range.java
+++ Range.java
@@ -1,1 +1,10 @@
-
+public class Range implements NumberGroup
+{
+    private int[] numbers;
+    public Range(int minVal, int maxVal)
+    {
+        for(int i = minVal; i <= maxVal; i++){
+            
+        }
+    }
+}
